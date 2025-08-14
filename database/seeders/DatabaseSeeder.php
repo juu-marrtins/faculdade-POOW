73e4f01a84364a831d17e587aa4870056c4ef9fb
Julia Martins <juliagabrielly.m.araujo@gmail.com>
2025-08-14 18:31:37 -0300

    Inicializando repository

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -11,9 +11,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        $this->call([
-            ProductSeeder::class
-        ]);
+        //
     }
 
 }
--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,7 @@
 <?php
 
-use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/products', [ProductController::class, 'index'])->name('products.index');
 Route::get('/', function () {
     return view('welcome');
 });
